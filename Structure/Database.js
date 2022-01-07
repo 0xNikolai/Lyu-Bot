e52fe04c5fb23aa774b70658cbfb6270396190d5
lyuhost <96669741+0xNikolai@users.noreply.github.com>
2022-01-07 18:46:29 -0500

    Update Database.js

--- Structure/Database.js
+++ Structure/Database.js
@@ -3,7 +3,7 @@ const Database = new mysql.createConnection({
     host: "localhost",
     user: "root",
     password: "",
-    database: ">_0xnikolai"
+    database: ""
 })
 
 Database.connect(function(err) {
